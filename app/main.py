1ee88f95cd7ae8dfba32630cf4e9df6ab95630e4
Geekki1245 <56454778+Geekki1245@users.noreply.github.com>
2025-03-09 21:49:37 -0300

    feat: generate_fake_email func added.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-from utils import generate_fake_names
+import secrets
+from typing import TypeVar
+
+from utils import generate_fake_names, generate_fake_email
+
 
 app = FastAPI()
+
+T = TypeVar("T")
 
 # Models
 class FakeUser(BaseModel):
@@ -11,9 +17,37 @@ class FakeUser(BaseModel):
     email: str
     password: str
 
+class SystemMessageResponse(BaseModel):
+    system_message: str
+    data: T
+
+class FakeUserResponse(SystemMessageResponse):
+    data: list[FakeUser]
+    
+
 @app.get("/")
-async def fake_user_data(n: int, unique: bool = True) -> list[FakeUser]:
+async def fake_user_data(n: int, unique: bool = True) -> FakeUserResponse:
     names = generate_fake_names(n, unique=unique)
 
-    return list(map(lambda i: {"name": i, "email": "email", "password": "password"}, names))
-    pass
+    fake_users = list(
+        map(
+            lambda i: 
+                {
+                "name": i, 
+                "email": generate_fake_email(i), 
+                "password": secrets.token_hex(16),
+                }, 
+            names
+        )
+    )
+    
+    if unique and len(fake_users) == 0:
+        message = "couldn't generate users due unique constraint. Max unique users already generated."
+    elif unique and len(fake_users) < n:
+        message = f"couldn't generate {n} users due unique constraint. Max unique users generated."
+    elif len(fake_users) < n:
+        message = "couldn't generate suficient users due some reason."
+    else:
+        message = f"{n} fake users generated."
+
+    return {"system_message": message, "data": fake_users}
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,7 @@
 import random
+import re
+
+from pydantic import BaseModel, field_validator
 
 
 names_registry: set = set()
@@ -31,4 +34,20 @@ def generate_fake_names(n_names: int, unique: bool = True) -> list[str]:
         generated_names.append(name)
 
     return generated_names
+
+def generate_fake_email(name: str) -> str:
+    email_makers = [
+        lambda f, l: l[0] +"."+ f +"@gmail.com",
+        lambda f, l: f + str(random.randint(0, 500)).zfill(3) + "@gmail.com",
+        lambda f, l: l + str(random.randint(0, 500)).zfill(3) + "@gmail.com",
+        lambda f, l: f +"."+ l +"@gmail.com"
+    ]
+
+    if not re.match(r'^\w+ \w+$', name):
+        raise ValueError('Name must be in the format "first_name last_name"')
+
+    choice = random.randint(0, len(email_makers) - 1)
+
+    first_name, last_name = name.split(" ")
     
+    return email_makers[choice](first_name, last_name)
